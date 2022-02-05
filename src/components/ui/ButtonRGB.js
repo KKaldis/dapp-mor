e6b231d2fa4f9c4f874b5373087eeb6abb4ea9c5
Kostas Kaldis <kostaskaldis@gmail.com>
2022-02-05 14:03:03 +0200

    cleanup

--- src/components/ui/ButtonRGB.js
+++ src/components/ui/ButtonRGB.js
@@ -1,6 +1,8 @@
 export default function ButtonRGB(props) {
   return (
     <div
+      message={props.message}
+      messageHover={props.messageHover}
       className={`rgbButton shineButton rounded ${props.className} `}
       onClick={props.onClick}
       onMouseEnter={props.onMouseEnter}
